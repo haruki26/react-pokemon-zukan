4a16b2beed8516e12d40334398b064a293d6dc46
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2024-10-05 15:10:03 +0900

    初期ページ作成

---
+++ src/pages/PokemonList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const PokemonList: React.FC = () => {
+    return (
+        <div className="text-center mt-10">
+            <h1 className="text-4xl font-bold text-blue-500">Reactポケモン図鑑リスト</h1>
+        </div>
+    );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { plugin } = require('postcss');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extends: {},
+  },
+  plugins: [],
+}
